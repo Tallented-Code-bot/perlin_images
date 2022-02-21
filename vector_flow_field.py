8d84fdbc6e854a7a27179194ecb1d9afd540e6e1
Calvin Tallent <calvintallentspam@gmail.com>
2022-02-21 14:05:09 -0800

    Turn code into a function that can be called.

--- vector_flow_field.py
+++ vector_flow_field.py
@@ -5,60 +5,61 @@ import tqdm
 import math
 
 
-# Intitialize constants.
-w, h = 1920, 1920
-shape = (h, w, 2)
-num_particles = 1000
-iterations = 100
+def create_image(
+    filename: str, w: int, h: int, base_color, seed: int = None, iterations: int = 100
+):
+    """Create an image based on perlin noise flow fields."""
+    # Set numpy seed if it is specified.
+    if seed is not None:
+        np.random.seed(seed)
 
-# Generate a 2d field of perlin noise.  The range
-# is -1 to 1.
-angle_field = generate_perlin_noise_2d((h, w), (16, 16)).astype(np.float64)
-angle_field = (angle_field + 1) * math.pi  # convert noise to angles between 0 and 2PI
+    # Generate a 2d field of perlin noise.  The range
+    # is -1 to 1.
+    angle_field = generate_perlin_noise_2d((h, w), (10, 10)).astype(np.float64)
+    angle_field = (
+        angle_field + 1
+    ) * math.pi  # convert noise to angles between 0 and 2PI
 
-# Turn the angles into a field of vectors
-field_x = np.cos(angle_field)
-field_y = np.sin(angle_field)
-field = np.stack((field_x, field_y), axis=2)
+    # Turn the angles into a field of vectors
+    field_x = np.cos(angle_field)
+    field_y = np.sin(angle_field)
+    field = np.stack((field_x, field_y), axis=2)
 
+    # Create an array of particles, one for every pixel in the image.
+    xarr = np.arange(w)
+    yarr = np.arange(h)
+    particles = np.array(np.meshgrid(xarr, yarr)).T.reshape(-1, 2)
 
-# Create an array of particles, one for every pixel in the image.
-xarr = np.arange(w)
-yarr = np.arange(h)
-particles = np.array(np.meshgrid(xarr, yarr)).T.reshape(-1, 2)
+    # Initialize the particle velocities.
+    particleVelocities = np.zeros(particles.shape)
 
-# Initialize the particle velocities.
-particleVelocities = np.zeros(particles.shape)
+    brightness = np.zeros((h, w))
 
+    # Iterate for the number of iterations (tqdm is a progress bar).
+    for i in tqdm.tqdm(range(iterations), desc="Loading..."):
+        # Transpose the particles array, then use it as an index
+        # to update the velocity with the vectors corresponding to the position of each particle.
+        transposedParticles = np.floor(particles).astype(np.int64).transpose()
+        particleVelocities = (
+            particleVelocities + field[transposedParticles[0], transposedParticles[1]]
+        )
 
-brightness = np.zeros((h, w))
+        # Update the particle positions.
+        particles = np.clip(particles + particleVelocities, 0, h - 1)
 
-# Iterate for the number of iterations (tqdm is a progress bar).
-for i in tqdm.tqdm(range(iterations), desc="Loading..."):
-    # Transpose the particles array, then use it as an index
-    # to update the velocity with the vectors corresponding to the position of each particle.
-    transposedParticles = np.floor(particles).astype(np.int64).transpose()
-    particleVelocities = (
-        particleVelocities + field[transposedParticles[0], transposedParticles[1]]
-    )
+        # Add 1 to each position where there is a particle.
+        brightness_to_add = np.zeros((h, w))
+        brightness_to_add[transposedParticles[0], transposedParticles[1]] = 1
+        brightness += brightness_to_add
 
-    # Update the particle positions.
-    particles = np.clip(particles + particleVelocities, 0, h - 1)
+    # Normalize the brightness array to a range of 0-1
+    brightness = brightness / np.max(brightness)
 
-    # Add 1 to each position where there is a particle.
-    brightness_to_add = np.zeros((h, w))
-    brightness_to_add[transposedParticles[0], transposedParticles[1]] = 1
-    brightness += brightness_to_add
+    # Apply a coloring function to create red, green, and blue values.
+    r = np.sqrt(brightness) * 180
+    g = np.full(brightness.shape, base_color)
+    b = np.sqrt(brightness) * 225
 
-
-# Normalize the brightness array to a range of 0-1
-brightness = brightness / np.max(brightness)
-
-# Apply a coloring function to create red, green, and blue values.
-r = np.sqrt(brightness) * 180
-g = np.full(brightness.shape, 20)
-b = np.sqrt(brightness) * 225
-
-# Write the rgb file to disk.
-rgb = np.transpose([r, g, b], (1, 2, 0)).astype(np.float32)
-cv2.imwrite("image.png", rgb)
+    # Write the rgb file to disk.
+    rgb = np.transpose([r, g, b], (1, 2, 0)).astype(np.float32)
+    cv2.imwrite(filename, rgb)
